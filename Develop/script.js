700d39439c46c81c6a7a8723185819387a67bd5d
dabyn <davyn.inthavong@gmail.com>
2023-04-08 23:55:21 -0500

    added needed variables

---
+++ Develop/script.js
@@ -0,0 +1,105 @@
+// Assignment Code
+
+// Main Function
+function generatePass() {
+    
+    // Need the following variables:
+    // array of lowercase letters
+    // array of uppercase letters
+    // array of numbers
+    // array of special characters
+    // to hold the password we're building
+    var lowercaseLetters = [
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "o",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "u",
+        "v",
+        "w",
+        "x",
+        "y",
+        "z",
+    ];
+    var uppercaseLetters = [
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "J",
+        "K",
+        "L",
+        "M",
+        "N",
+        "O",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "U",
+        "V",
+        "W",
+        "X",
+        "Y",
+        "Z",
+    ];
+    var numChar = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+    var specialChar = [
+        "!",
+        "#",
+        "$",
+        "%",
+        "&",
+        "'",
+        "(",
+        ")",
+        "*",
+        "+",
+        ",",
+        "-",
+        ".",
+        "/",
+        ":",
+        ";",
+        "<",
+        "=",
+        ">",
+        "?",
+        "@",
+        "[",
+        "_",
+        "]",
+        "^",
+        "|",
+        "`",
+        "{",
+        "|",
+        "}",
+        "~",
+    ];
+    var selectedArray = [];
+    var createdPass = "";
+
+}
